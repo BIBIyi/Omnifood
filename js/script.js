bebf987024a1d36a3131900c6507f8f6141a1af6
BIBIyi <385115337@qq.com>
2022-09-23 22:26:45 +1000

    add js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,81 @@
 console.log("hello world!");
+const myName = "emi";
+const h1 = document.querySelector(".heading-primary");
+console.log(myName);
+console.log(h1);
+
+//////////////////////////////////////////////////////////
+///set current year
+//////////////////////////////////////////////////////////
+const yearEL = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEL.textContent = currentYear;
+
+//////////////////////////////////////////////////////////
+/// Make mobile navigation work
+//////////////////////////////////////////////////////////
+
+const btnNAvEl = document.querySelector(".btn-mobile-nav");
+const headerEL = document.querySelector(".header");
+
+btnNAvEl.addEventListener("click", function () {
+  headerEL.classList.toggle("nav-open");
+});
+
+///////////////////////////////////////////////////////////
+// Smooth scrolling animation
+
+const allLInks = document.querySelectorAll("a:link");
+
+allLInks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile naviagtion
+    if (link.classList.contains("main-nav-link")) {
+      headerEL.classList.toggle("nav-open");
+    }
+  });
+});
+
+///////////////////////////////////////////////////////////
+// Sticky navigation 当页面下移后再出现固定导航,
+const sectionHeroEl = document.querySelector(".section-hero");
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+      // document.querySelector(".header").classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
